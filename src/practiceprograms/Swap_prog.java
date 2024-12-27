3b3d7a9a4d4dfb68df646b962e3792545c3501b2
Swetha11-code <swetha.qa2024@gmail.com>
2024-12-27 14:17:44 +0530

    added new practiceprograms folder

---
+++ src/practiceprograms/Swap_prog.java
@@ -0,0 +1,34 @@
+package practiceprograms;
+
+public class Swap_prog
+{
+	public static void main(String[] args)
+	{
+	/*	int x = 45;
+		int y = 95;
+		
+			System.out.println("Before swap x value  = "  + x +   " After swap y value = "+y);
+		
+		x= x+y;//45+95 = 150
+		y= x-y;//150-95=45
+		y=x-y;//150-45=95
+		
+		System.out.println("After swap x value =   "+ x +  " After swap y value = "+y);
+	*/
+		
+		int m= 85;
+		int n = 95;
+		{
+		System.out.println("Before swap m = " + m+ " Before swap n = "+n );
+		}
+		m=m+n;
+		n=m-n;
+		m=m-n;
+		{
+			System.out.println("After swap m = " +m+ " Before swap n = "+n);
+		}
+		
+		
+	}
+
+}
