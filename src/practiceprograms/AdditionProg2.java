b5129bf2bd61782db070fe74e0419407bcde90b9
Swetha11-code <swetha.qa2024@gmail.com>
2025-01-06 16:55:31 +0530

    child branch code

---
+++ src/practiceprograms/AdditionProg2.java
@@ -0,0 +1,13 @@
+package practiceprograms;
+
+public class AdditionProg
+{
+	public static void main(String[] args) 
+	{
+		int a=25,b=65,c=95;
+		System.out.println(a+b+c);
+System.out.println(65+25);
+System.out.println(" the sum result = "+(a+b+c));
+	}
+
+}
